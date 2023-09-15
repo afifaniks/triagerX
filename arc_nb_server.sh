1bc53ba8c0e89bad6c9d895bb3b6eed881ff6886
Afif Al Mamun <afifanik@gmail.com>
2023-09-14 23:37:48 -0600

    Update slurm script

--- arc_nb_server.sh
+++ arc_nb_server.sh
@@ -2,8 +2,10 @@
 #SBATCH -J JupyterServer
 #SBATCH -t 5:00:00
 #SBATCH -n 1
-#SBATCH --mem 0
+#SBATCH --mem=40G
 #SBATCH --partition=gpu-a100 --gres=gpu:1
+#SBATCH --output=./slurm_out/%j.out
 
-conda activate triagerx
+source ~/miniconda3/etc/profile.d/conda.sh && \
+conda activate triagerx && \
 jupyter notebook --no-browser --ip="*" --port=9779 --NotebookApp.token=letmepass777 --notebook-dir="/home/mdafifal.mamun/notebooks/triagerX/notebook"
