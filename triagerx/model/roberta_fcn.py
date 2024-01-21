409a87c516cd11e84b2d9a55c13aa6041691751e
Afif Al Mamun <afifanik@gmail.com>
2024-01-20 22:30:13 -0700

    Add fcn roberta model

---
+++ triagerx/model/roberta_fcn.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+from transformers import AutoModel, AutoTokenizer, PreTrainedTokenizer
+
+
+class RobertaFCNClassifier(nn.Module):
+    def __init__(
+        self, 
+        model_name: str, 
+        output_size, 
+        embed_size=1024, 
+        dropout=0.1
+    ) -> None:
+        super().__init__()
+        self.base = AutoModel.from_pretrained(model_name)
+        self._tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.linear = nn.Linear(embed_size, embed_size // 2)
+        self.linear2 = nn.Linear(embed_size // 2, output_size)
+        self.dropout = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+        self.relu2 = nn.ReLU()
+
+    def forward(self, input_id, attention_mask):
+        _, pooler_out = self.base(
+            input_ids=input_id, attention_mask=attention_mask, return_dict=False
+        )
+        pooler_out = self.relu(pooler_out)
+        drop_out = self.dropout(pooler_out)
+        linear_out = self.linear(drop_out)
+        linear_out = self.relu(linear_out)
+        drop_out = self.dropout2(linear_out)
+        linear_out = self.linear2(drop_out)
+
+        return linear_out
+
+    def tokenizer(self) -> PreTrainedTokenizer:
+        return self._tokenizer
