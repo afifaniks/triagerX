cc834eee34010f058412cea9b702ee794c1e94dc
Afif Al Mamun <afifanik@gmail.com>
2024-03-16 10:56:44 -0600

    Add new issue_dump

--- util/issue_dump.py
+++ util/issue_dump.py
@@ -25,15 +25,25 @@ def dump_issues(owner, repo, gh_username, gh_token, path):
         for issue in issues:
             print(f"Extracting issue data: {issue['number']}")
             timeline_url = issue['timeline_url']
+
+            comments_url = issue['comments_url']
+
             timeline_response = requests.get(timeline_url, auth=(gh_username, gh_token))
             timeline_data = timeline_response.json()
 
             issue["timeline_data"] = timeline_data
 
+            comments_response = requests.get(comments_url, auth=(gh_username, gh_token))
+            comments_data = comments_response.json()
+
+            issue["comments_data"] = comments_data
+
             with open(os.path.join(path, f"{page}_{issue['number']}.json"), "w") as file:
                 file.write(json.dumps(issue, indent=2))
 
-            time.sleep(2)
+            time.sleep(1.2)
+
+        page += 1
 
 
 # Example usage
