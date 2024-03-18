7ec6863b6708c599837809d70d74763c362e7608
Afif Al Mamun <afifanik@gmail.com>
2024-03-18 00:16:34 -0600

    Add exceptions and update header

--- util/issue_dump.py
+++ util/issue_dump.py
@@ -12,10 +12,17 @@ def dump_issues(owner, repo, gh_username, gh_token, path):
     page = 1
     per_page = 100
     base_url = f"https://api.github.com/repos/{owner}/{repo}"
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28',
+        'Authorization': f'Bearer {gh_token}'
+    }
 
     while True:
         issues_url = f"{base_url}/issues?state=all&page={page}&per_page={per_page}"
         issues_response = requests.get(issues_url, auth=(gh_username, gh_token))
+        if issues_response.status_code != 200:
+            raise Exception(f"Error occurred: {issues_response.status_code}\n{issues_response}")
         issues = issues_response.json()
 
         if len(issues) == 0:
@@ -23,17 +30,25 @@ def dump_issues(owner, repo, gh_username, gh_token, path):
             break
 
         for issue in issues:
+            if "/pull/" in issue["html_url"]:
+                print("Pull request found. Skipping...")
+                continue
+
             print(f"Extracting issue data: {issue['number']}")
             timeline_url = issue['timeline_url']
 
             comments_url = issue['comments_url']
 
-            timeline_response = requests.get(timeline_url)
+            timeline_response = requests.get(timeline_url, headers=headers)
+            if timeline_response.status_code != 200:
+                raise Exception(f"Error occurred: {timeline_response.status_code}\n{timeline_response}")
             timeline_data = timeline_response.json()
 
             issue["timeline_data"] = timeline_data
 
             comments_response = requests.get(comments_url, auth=(gh_username, gh_token))
+            if comments_response.status_code != 200:
+                raise Exception(f"Error occurred: {comments_response.status_code}\n{comments_response}")
             comments_data = comments_response.json()
 
             issue["comments_data"] = comments_data
