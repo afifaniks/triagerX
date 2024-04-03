4097e204ddb765c6e999034f4da20095ab06de10
Afif Al Mamun <afifanik@gmail.com>
2024-04-03 17:00:52 -0600

    Update alloc script

--- scripts/alloc.sh
+++ scripts/alloc.sh
@@ -2,9 +2,22 @@
 
 if [ "$1" = "gpu" ]
 then
-    echo "Allocating GPU partition..."
-    salloc --mem=50G -t 04:59:00 -p disa --gres=gpu:1
+    partition="disa"
+elif [ "$1" = "cpu" ]
+then
+    partition="cpu2019-bf05"
 else
-    echo "Allocating CPU partition..."
-    salloc --mem=50G -c 1 -N 1 -n 1 -t 04:00:00 -p cpu2019-bf05
+    echo "Invalid argument. Usage: $0 gpu|cpu [partition]"
+    exit 1
+fi
+
+selected_partition=${2:-$partition}
+
+if [ "$1" = "gpu" ]
+then
+    echo "Allocating GPU partition $selected_partition..."
+    salloc --mem=50G -t 04:59:00 -p "$selected_partition" --gres=gpu:1
+else
+    echo "Allocating CPU partition $selected_partition..."
+    salloc --mem=50G -c 1 -N 1 -n 1 -t 04:00:00 -p "$selected_partition"
 fi
