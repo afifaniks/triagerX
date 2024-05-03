c050d844cfecb13cabf5044e6eea506c9aade508
Afif Al Mamun <afifanik@gmail.com>
2024-05-02 21:07:01 -0600

    Use CUDA for processing

--- triagerx/system/triagerx.py
+++ triagerx/system/triagerx.py
@@ -23,10 +23,14 @@ class TriagerX:
             train_data: pd.DataFrame,
             developer_id_map: Dict[str, int],
             component_id_map: Dict[str, int],
-            expected_deveopers: List[str]
+            component_developers_map: Dict[str, List[str]],
+            device: str
             ) -> None:
         self._component_prediction_model = component_prediction_model
         self._developer_prediction_model = developer_prediction_model
+        self._device = device
+        self._component_prediction_model = self._component_prediction_model.to(device)
+        self._developer_prediction_model = self._developer_prediction_model.to(device)
         self._similarity_model = similarity_model
         self._tokenizer = tokenizer
         self._tokenizer_config = tokenizer_config
@@ -43,38 +47,44 @@ class TriagerX:
         self._component2id_map = component_id_map
         self._id2developer_map = {idx: dev for dev, idx in developer_id_map.items()}
         self._id2component_map = {idx: comp for comp, idx in component_id_map.items()}
-        self._expected_deveopers = expected_deveopers
-        logger.debug("Generating embedding for training data...")
-        self._all_embeddings = similarity_model.encode(self._train_data.raw_text.to_list(), batch_size=15)
+        self._component_developers_map = component_developers_map
+        self._expected_developers =  [user for user_list in self._component_developers_map.values() for user in user_list]
+        logger.debug("Generating embedding for existing issues...")
+        self._all_embeddings = similarity_model.encode(self._train_data.raw_text.to_list(), batch_size=15)        
 
-    def get_recommendation(self, issue, k=3):
+    def get_recommendation(self, issue, k_comp=3, k_dev=5):
         logger.debug("Processing issue...")
         cleaned_issue = self._clean_data(issue_data=issue)
         processed_issue = self._process_issues(issue=cleaned_issue)
 
         logger.debug("Prediciting components...")
-        comp_prediction_score, predicted_components = self._get_predicted_components(tokenized_issue=processed_issue, k=k)
-        print(predicted_components)
-        predicted_components = predicted_components.squeeze(dim=0).cpu().numpy().tolist()
+        comp_prediction_score, predicted_components = self._get_predicted_components(tokenized_issue=processed_issue, k=k_comp)
         predicted_components_name = [self._id2component_map[idx] for idx in predicted_components]
         logger.info(f"Predicted components: {predicted_components_name}")
 
         logger.debug("Generating developer recommendation...")
-        dev_prediction_score, predicted_devs = self._get_recommendation_from_dev_model(tokenized_issue=processed_issue, k=5)
-        predicted_devs = predicted_devs.squeeze(dim=0).cpu().numpy().tolist()
-        dev_prediction_score = dev_prediction_score.squeeze(dim=0).cpu()
+        dev_prediction_score, predicted_devs = self._get_recommendation_from_dev_model(tokenized_issue=processed_issue, k=k_dev)
         predicted_developers_name = [self._id2developer_map[idx] for idx in predicted_devs]
         logger.info(f"Recommended developers: {predicted_developers_name}\nSoftmax: {dev_prediction_score}")
 
         dev_predictions_by_similarity = self._get_recommendation_by_similarity(
             issue, 
             predicted_components, 
-            k_dev=5, 
+            k_dev=k_dev, 
             k_issue=10, 
             similarity_threshold=0.5
         )
 
-        return predicted_devs, dev_predictions_by_similarity, predicted_components_name
+        recommendations = {
+            "predicted_components": predicted_components_name,
+            "comp_prediction_score": comp_prediction_score,
+            "predicted_developers": predicted_developers_name,
+            "comp_prediction_score": dev_prediction_score,
+            "similar_devs": dev_predictions_by_similarity
+            
+        }
+
+        return recommendations
 
     def _process_issues(self, issue: str):
         return self._tokenizer(
@@ -88,25 +98,35 @@ class TriagerX:
     def _get_recommendation_from_dev_model(self, tokenized_issue, k):
         with torch.no_grad():
             predictions = self._developer_prediction_model(
-                input_ids=tokenized_issue["input_ids"],
-                tok_type=tokenized_issue["token_type_ids"],
-                attention_mask=tokenized_issue["attention_mask"]
+                input_ids=tokenized_issue["input_ids"].to(self._device),
+                tok_type=tokenized_issue["token_type_ids"].to(self._device),
+                attention_mask=tokenized_issue["attention_mask"].to(self._device)
             )
 
-        output = torch.sum(torch.stack(predictions), 0)
-        
-        return output.topk(k, 1, True, True)
+        output = torch.sum(torch.stack(predictions), 0)        
+        prediction_score, predicted_devs = output.topk(k, 1, True, True)
+
+        predicted_devs = predicted_devs.squeeze(dim=0).cpu().numpy().tolist()
+        prediction_score = prediction_score.squeeze(dim=0).cpu().numpy().tolist()
+
+        return prediction_score, predicted_devs
+
     
     def _get_predicted_components(self, tokenized_issue, k):
         with torch.no_grad():
             predictions = self._component_prediction_model(
-                input_ids=tokenized_issue["input_ids"],
-                tok_type=tokenized_issue["token_type_ids"],
-                attention_mask=tokenized_issue["attention_mask"]
+                input_ids=tokenized_issue["input_ids"].to(self._device),
+                tok_type=tokenized_issue["token_type_ids"].to(self._device),
+                attention_mask=tokenized_issue["attention_mask"].to(self._device)
             )
 
-        output = torch.sum(torch.stack(predictions), 0)
-        return output.topk(k, 1, True, True)
+        output = torch.sum(torch.stack(predictions), 0)        
+        prediction_score, predicted_components = output.topk(k, 1, True, True)
+
+        predicted_components = predicted_components.squeeze(dim=0).cpu().numpy().tolist()
+        prediction_score = prediction_score.squeeze(dim=0).cpu().numpy().tolist()
+
+        return prediction_score, predicted_components
     
     def _get_recommendation_by_similarity(self, issue, predicted_components, k_dev, k_issue, similarity_threshold):
         similar_issues = self._get_top_k_similar_issues(issue, k=k_issue, threshold=similarity_threshold)
@@ -117,40 +137,38 @@ class TriagerX:
 
         logger.debug(historical_contribution)
 
-        top_k_devs = [contrib[0] for contrib in historical_contribution[:k_dev]]
+        top_k_devs = historical_contribution[:k_dev]
 
         return top_k_devs
 
     def _get_historical_contributors(self, similar_issues, predicted_component_ids):
         user_contribution_counts = {}
+        skipped_users = []
 
         for issue_index, sim_score in similar_issues:
             base_points = sim_score
             
             issue = self._train_data.iloc[issue_index]
-            # print("Issue Number", df_train.iloc[issue_index].issue_number, df_train.iloc[issue_index].issue_title)
-
-            # print(f"Issue label: {label2idx[issue.component]} -- Predicted: {predicted_component_ids}")
 
             if self._component2id_map[issue.component] not in predicted_component_ids:
                 logger.warning(f"Skipping issue as label id {self._component2id_map[issue.component]} did not match with any of {predicted_component_ids}")
                 continue
 
             issue_number = issue.issue_number
-            contributors = self._get_contribution_data(issue_number)
+            contributors = self._get_contribution_data(issue_number)            
 
             for key, users in contributors.items():
                 for user in users:
-                    if user not in self._expected_deveopers:
-                        logger.warning(f"Skipping: {user} because it doesn't exist in {{expected_developers}} list")
+                    if user not in self._expected_developers:
+                        skipped_users.append(user)                        
                         continue            
                                 
-                    # if user in components[issue.component]:
-                    #     user_contribution_counts[user] = user_contribution_counts.get(user, 0) + base_points * 1.25
-                    # else:
-                    user_contribution_counts[user] = user_contribution_counts.get(user, 0) + base_points
+                    if user in self._component_developers_map[issue.component]:
+                        user_contribution_counts[user] = user_contribution_counts.get(user, 0) + base_points * 1.25
+                    else:
+                        user_contribution_counts[user] = user_contribution_counts.get(user, 0) + base_points
             
-            # base_points /= 2.0
+        logger.warning(f"Skipped users: {skipped_users} because they don't exist in the ${{expected_developers}} list")
         
         user_contribution_counts = sorted(user_contribution_counts.items(), key=lambda x: x[1], reverse=True)
         return user_contribution_counts
