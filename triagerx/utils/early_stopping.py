1e8e244950b9e8c810737067df888ff8a3dad462
Afif Al Mamun <afifanik@gmail.com>
2024-05-15 15:01:55 -0600

    Implement early stopping

---
+++ triagerx/utils/early_stopping.py
@@ -0,0 +1,42 @@
+from loguru import logger
+
+
+class EarlyStopping:
+    def __init__(self, patience=5, verbose=True, delta=0):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 5
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                           Default: 0
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.delta = delta
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = float("inf")
+
+    def __call__(self, val_loss):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.val_loss_min = val_loss
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                logger.debug(
+                    f"EarlyStopping counter: {self.counter} out of {self.patience}"
+                )
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.val_loss_min = val_loss
+            self.counter = 0
+            if self.verbose:
+                logger.debug(f"Validation loss improved to {val_loss:.6f}")
