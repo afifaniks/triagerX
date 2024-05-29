60d1eabfd6ad3b7cdeb67d06f4a62a88634b3b5f
Afif Al Mamun <afifanik@gmail.com>
2024-05-29 13:04:26 -0600

    Update API description

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ from dto.recommendation_response import RecommendationResponse
 from service.recommendation_service import RecommendationService
 from util.config_loader import ConfigLoader
 
-app = FastAPI()
+
 config_loader = ConfigLoader("config/triagerx_config.yaml")
-recommendation_service = RecommendationService(config_loader.get_config())
+configs = config_loader.get_config()
+
+app = FastAPI(
+    title=configs["project"]["title"], version=configs["project"]["api_version"]
+)
+
+logger.debug("Initializing recommendation service...")
+recommendation_service = RecommendationService(configs)
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def health_check():
+    return {"status": 200}
 
 
-@app.post("/recommendation")
-def read_item(request: RecommendationRequest) -> RecommendationResponse:
+@app.post(
+    "/recommendation",
+    description="Get component and developer recommendations for Openj9 Issues",
+)
+def get_recommendation(request: RecommendationRequest) -> RecommendationResponse:
     logger.debug(f"Received request: {request}")
     recommendations = recommendation_service.get_recommendation(
         request.issue_tile, request.issue_description
