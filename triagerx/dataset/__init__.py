901fd924042a0bb57bd8b1de2d36f190280a6c8b
Afif Al Mamun <afifanik@gmail.com>
2024-06-05 16:31:41 -0600

    Add ensemble dataset

---
+++ triagerx/dataset/__init__.py
@@ -0,0 +1,4 @@
+from .ensemble_dataset import EnsembleDataset
+from .triage_dataset import TriageDataset
+
+__all__ = ["EnsembleDataset", "TriageDataset"]
---
+++ triagerx/dataset/ensemble_dataset.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+from loguru import logger
+from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer
+
+
+class EnsembleDataset(Dataset):
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        tokenizer1: PreTrainedTokenizer,
+        tokenizer2: PreTrainedTokenizer,
+        feature: str,
+        target: str,
+        max_length: int = 512,
+    ):
+        logger.debug("Generating torch dataset...")
+        logger.debug(f"Dataset feature column: {feature}, target column: {target}")
+        self.tokenizer1 = tokenizer1
+        self.tokenizer2 = tokenizer2
+        self.labels = [label for label in df[target]]
+        logger.debug("Tokenizing texts...")
+        self.texts = [
+            (
+                self.tokenizer1(
+                    row[feature],
+                    padding="max_length",
+                    max_length=max_length,
+                    truncation=True,
+                    return_tensors="pt",
+                ),
+                self.tokenizer2(
+                    row[feature],
+                    padding="max_length",
+                    max_length=max_length,
+                    truncation=True,
+                    return_tensors="pt",
+                ),
+            )
+            for _, row in df.iterrows()
+        ]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
