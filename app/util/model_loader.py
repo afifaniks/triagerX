e74a24e6f06fcf3ca80d2b65afcb0191f59a462c
Afif Al Mamun <afifanik@gmail.com>
2024-06-06 12:13:40 -0600

    Refactor app

---
+++ app/util/model_loader.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from transformers import PreTrainedTokenizer
+
+from triagerx.model.lbt_p_deberta import LBTPDeberta
+
+
+def get_trained_model(
+    weight_path: str,
+    num_classes: int,
+    unfrozen_layers: int,
+    dropout: float,
+    base_model: str,
+    tokenizer: PreTrainedTokenizer,
+    device: str,
+) -> nn.Module:
+    model = LBTPDeberta(
+        output_size=num_classes,
+        unfrozen_layers=unfrozen_layers,
+        num_classifiers=unfrozen_layers,
+        dropout=dropout,
+        base_model=base_model,
+    )
+
+    model.base_model.resize_token_embeddings(len(tokenizer))
+
+    model.load_state_dict(torch.load(weight_path, map_location=device))
+
+    return model
