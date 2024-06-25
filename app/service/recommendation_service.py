7e701e732cdfe3e3265a7437a4abb24e683b9980
Afif Al Mamun <afifanik@gmail.com>
2024-06-25 13:30:03 -0600

    Refactor service

--- app/service/recommendation_service.py
+++ app/service/recommendation_service.py
@@ -1,14 +1,15 @@
+import os
 from typing import Any, Dict
 
+import numpy as np
 import pandas as pd
 import torch
 from loguru import logger
 from sentence_transformers import SentenceTransformer
-from transformers import AutoTokenizer
 
+from app.util.model_loader import get_trained_model
 from triagerx.dataset.text_processor import TextProcessor
 from triagerx.system.triagerx import TriagerX
-from util.model_loader import get_trained_model
 
 
 class RecommendationService:
@@ -33,58 +34,55 @@ class RecommendationService:
             self._config["similarity_model"]["model_key"]
         )
 
-    def _initialize_triager(self):
-        tokenizer = AutoTokenizer.from_pretrained(
-            self._config["tokenizer"]["model_name"]
-        )
-        tokenizer_config = self._config["tokenizer"]
-        developer_id_map = self._config["developer_id_map"]
-        component_id_map = self._config["component_id_map"]
-        component_dev_map = self._config["component_developer_map"]
-        train_df = pd.read_csv(self._config["data"]["train_data"])
-        issues_dir = self._config["data"]["issues_dir"]
+        embeddings_path = self._config["similarity_model"]["embeddings_path"]
+        if not os.path.exists(embeddings_path):
+            logger.debug("Historical issue embedding doesn't exist, generating new...")
+            df_train = pd.read_csv(self._config["data"]["train_data"])
+            encodings = self._similarity_model.encode(
+                df_train.text.tolist(), show_progress_bar=True
+            )
+            np.save(embeddings_path, encodings)
 
+    def _initialize_triager(self):
         logger.debug("Initializing Triager X engine...")
         self.triager = TriagerX(
-            developer_prediction_model=self._developer_model,
             component_prediction_model=self._component_model,
-            similarity_model=self._similarity_model,
-            tokenizer=tokenizer,
-            tokenizer_config=tokenizer_config,
-            train_data=train_df,
-            developer_id_map=developer_id_map,
-            component_id_map=component_id_map,
-            component_developers_map=component_dev_map,
-            issues_path=issues_dir,
+            developer_prediction_model=self._developer_model,
+            similarity_model=self._developer_model,
+            train_data=pd.read_csv(self._config["data"]["train_data"]),
+            train_embeddings=self._config["similarity_model"]["embeddings_path"],
+            issues_path=self._config["data"]["issues_path"],
+            developer_id_map=self._config["developer_id_map"],
+            component_id_map=self._config["component_id_map"],
+            expected_developers=set(self._config["developer_id_map"].keys()),
             device=self._device,
+            similarity_prediction_weight=self._config["contribution_score_params"][
+                "similarity_prediction_weight"
+            ],
+            time_decay_factor=self._config["contribution_score_params"][
+                "time_decay_factor"
+            ],
+            direct_assignment_score=self._config["contribution_score_params"][
+                "direct_assignment_score"
+            ],
+            contribution_score=self._config["contribution_score_params"][
+                "contribution_score"
+            ],
+            discussion_score=self._config["contribution_score_params"][
+                "discussion_score"
+            ],
         )
 
     def _load_trained_model(self, model_config: Dict):
-        logger.debug(
-            f"Loading pretrained model: {model_config['weights_save_location']}"
-        )
-        tokenizer = AutoTokenizer.from_pretrained(
-            model_config["base_transformer_model"]
-        )
-        return get_trained_model(
-            weight_path=model_config["weights_save_location"],
-            num_classes=model_config["num_classes"],
-            unfrozen_layers=model_config["unfrozen_layers"],
-            dropout=model_config["dropout"],
-            base_model=model_config["base_transformer_model"],
-            tokenizer=tokenizer,
-            device=self._device,
-        )
+        logger.debug(f"Loading pretrained model: {model_config['weights_path']}")
+        return get_trained_model(model_config)
 
     def get_recommendation(self, issue_title: str, issue_description: str):
-        processed_issue = TextProcessor.prepare_text(
-            issue_title,
-            issue_description,
-            summary="",
-            use_description=True,
-            use_summary=False,
-            use_special_tokens=False,
-        )
+        processed_issue = TextProcessor.prepare_text(issue_title, issue_description)
         return self.triager.get_recommendation(
-            processed_issue, k_comp=3, k_dev=3, k_rank=5, similarity_threshold=0.5
+            processed_issue,
+            k_comp=3,
+            k_dev=3,
+            k_rank=self._config["maximum_similar_issues"],
+            similarity_threshold=self._config["similarity_threshold"],
         )
