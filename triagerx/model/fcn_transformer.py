c78648f8db68d75c64f13d04479998868a39ed55
Afif Al Mamun <afifanik@gmail.com>
2024-07-02 12:20:28 -0600

    Refactor model

--- triagerx/model/fcn_transformer.py
+++ triagerx/model/fcn_transformer.py
@@ -1,7 +1,11 @@
 import torch
 from loguru import logger
 from torch import nn
-from transformers import AutoModel, AutoTokenizer, PreTrainedTokenizer
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    PreTrainedTokenizer,
+)
 
 from triagerx.model.prediction_model import PredictionModel
 
@@ -17,8 +21,8 @@ class FCNTransformer(PredictionModel):
         label_map=None,
     ) -> None:
         super(FCNTransformer, self).__init__()
-        self.base_model = AutoModel.from_pretrained(
-            base_model, output_hidden_states=True
+        self.base_model = AutoModelForSequenceClassification.from_pretrained(
+            base_model, num_labels=output_size
         )
         self._tokenizer = AutoTokenizer.from_pretrained(base_model)
         self._label_map = label_map
@@ -40,13 +44,6 @@ class FCNTransformer(PredictionModel):
                 for p in self.base_model.encoder.layer[i].parameters():
                     p.requires_grad = False
 
-        self._max_tokens = max_tokens
-        self._embed_size = self.base_model.config.hidden_size
-        self.unfrozen_layers = unfrozen_layers
-
-        self.dropout = nn.Dropout(dropout)
-        self.classifier = nn.Linear(self._embed_size * self._max_tokens, output_size)
-
     def forward(self, inputs):
         inputs = {
             key: value.squeeze(1).to(next(self.parameters()).device)
@@ -54,14 +51,8 @@ class FCNTransformer(PredictionModel):
         }
 
         base_out = self.base_model(**inputs)
-        hidden_state = base_out.hidden_states[-1]
 
-        batch_size, sequence_length, hidden_size = hidden_state.size()
-        x = hidden_state.view(batch_size, -1)
-        x = self.dropout(x)
-        x = self.classifier(x)
-
-        return x
+        return base_out.logits
 
     def tokenizer(self) -> PreTrainedTokenizer:
         return self._tokenizer
