2f9c78682c5b12dbd4e73ce3e9084c679a7a4889
Afif Al Mamun <afifanik@gmail.com>
2024-08-04 16:09:48 -0600

    Remove scripts

--- scripts/nb_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/miniconda3/etc/profile.d/conda.sh && \
-conda activate trx && \
-jupyter notebook --no-browser --ip="*" --port=9779 --NotebookApp.token=777
--- scripts/stop_nb_server_job.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scancel  $(squeue -u $USER | tail -1| awk '{print $1}')
